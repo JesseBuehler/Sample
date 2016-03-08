91322d4b3838e99d36d9e5d564f7d4720358ca2a
Jesse Buehler <jessebuehler@Jesses-MBP.home>
2016-03-07 20:19:35 -0500

    added test for menu items

--- weather.com/initial_integration.rb
+++ weather.com/initial_integration.rb
@@ -16,4 +16,15 @@ describe "Homepage" do
   	element = @driver.find_element(:id => "wx-twc-logo-img")
   end
 
+  it 'displays valid menu items' do
+  	elements = @driver.find_elements(:class => "header-menu__link")
+  	expect(elements[0].text).to eq('FORECASTS')
+  	expect(elements[1].text).to eq('MAPS')
+  	expect(elements[2].text).to eq('VIDEO')
+  	expect(elements[3].text).to eq('PHOTOS')
+  	expect(elements[4].text).to eq('NEWS')
+  	expect(elements[5].text).to eq('TV')
+  	expect(elements[6].text).to eq('WINTER STORMS')
+	end
+
 end
